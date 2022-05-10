c3ae170ce78880b051cd56ad221f3939653ceded
jluna101 <jfluna1996@gmail.com>
2022-05-09 21:57:46 -0700

    Added styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Timer from "./components/Timer/Timer.js";
 import TaskList from "./components/TaskList/TaskList.js";
 import Metrics from "./components/Metrics/Metrics.js";
 import React, { useState, useEffect } from "react";
+import Footer from "./components/Footer/Footer";
+import FooterTwo from "./components/Footer/FooterTwo"
+// import { DOM_KEY_LOCATION } from "@testing-library/user-event/dist/keyboard/types";
+
 
 function App() {
 	const [currentTimer, setCurrentTimer] = useState(null);
@@ -13,6 +17,7 @@ function App() {
 	const [pomsComplete, setPomsComplete] = useState(0);
 	const [breaksCounter, setBreaksCounter] = useState(0);
 	return (
+		<div>
 		<div className="app-container">
 			<Timer
 				currentTimer={currentTimer}
@@ -32,6 +37,9 @@ function App() {
 				breaksCounter={breaksCounter}
 				session={currentTimer}
 			/>
+			<Footer/>
+			<FooterTwo />
+			</div>
 		</div>
 	);
 }
